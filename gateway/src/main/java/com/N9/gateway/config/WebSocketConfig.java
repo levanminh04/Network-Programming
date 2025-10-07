c3cd0aa76ae2c729cddfc832b6d9c8715878e30f
levanminh04 <levanminh031204@gmail.com>
2025-10-08 01:37:21 +0700

    update

--- gateway/src/main/java/com/N9/gateway/config/WebSocketConfig.java
+++ gateway/src/main/java/com/N9/gateway/config/WebSocketConfig.java
@@ -1,6 +1,6 @@
 package com.N9.gateway.config;
 
-import com.n9.gateway.handler.AuthenticationHandler;
+import com.N9.gateway.handler.AuthenticationHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
